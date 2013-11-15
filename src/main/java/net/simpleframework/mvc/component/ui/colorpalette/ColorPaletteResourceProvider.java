21ddd98c6ea5c7008f18948112d796f533909c0c
simpleframework <cknet@126.com>
2013-11-15 11:32:35 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/mvc/component/ui/colorpalette/ColorPaletteResourceProvider.java
+++ src/main/java/net/simpleframework/mvc/component/ui/colorpalette/ColorPaletteResourceProvider.java
@@ -9,7 +9,7 @@ import net.simpleframework.mvc.component.ui.slider.SliderRegistry;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class ColorPaletteResourceProvider extends AbstractComponentResourceProvider {
--- src/main/java/net/simpleframework/mvc/component/ui/dictionary/AbstractDictionaryHandler.java
+++ src/main/java/net/simpleframework/mvc/component/ui/dictionary/AbstractDictionaryHandler.java
@@ -10,7 +10,7 @@ import net.simpleframework.mvc.component.ui.window.AbstractWindowHandler;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractDictionaryHandler extends AbstractWindowHandler implements
