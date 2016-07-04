fbb2c28790586047ea524e997568f5f920fbe0d0
CHENK <CHENK@CHENK-PC>
2016-07-04 16:32:31 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/component/ui/swfupload/SwfUploadUtils.java
+++ src/main/java/net/simpleframework/mvc/component/ui/swfupload/SwfUploadUtils.java
@@ -84,7 +84,8 @@ public abstract class SwfUploadUtils {
 					.append("<\\/span>\",");
 			sb.append("button_text_style: \".swf-button { color: #014060; text-align: center; }\",");
 			sb.append("button_text_top_padding: ")
-					.append(cp.getUserAgent().indexOf("AppleWebKit/") > -1 ? 4 : 2).append(",");
+					.append(cp.getRequestHeader("User-Agent").indexOf("AppleWebKit/") > -1 ? 4 : 2)
+					.append(",");
 		}
 		if (swfUpload.isMultiFileSelected()) {
 			sb.append("button_action: SWFUpload.BUTTON_ACTION.SELECT_FILES,");
