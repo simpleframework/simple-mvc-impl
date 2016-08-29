4f3ea85964f9ed9d7d285a03beb438de1e0561c0
CHENK <CHENK@CHENK-PC>
2016-08-29 22:14:37 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/component/ui/imageslide/ImageSlideResourceProvider.java
+++ src/main/java/net/simpleframework/mvc/component/ui/imageslide/ImageSlideResourceProvider.java
@@ -1,22 +1,18 @@
 package net.simpleframework.mvc.component.ui.imageslide;
 
 import net.simpleframework.mvc.PageParameter;
-import net.simpleframework.mvc.component.IComponentResourceProvider.AbstractComponentResourceProvider;
+import net.simpleframework.mvc.component.ui.AbstractComponentUIResourceProvider;
 import net.simpleframework.mvc.component.ui.menu.MenuRegistry;
 import net.simpleframework.mvc.impl.DefaultPageResourceProvider;
 
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class ImageSlideResourceProvider extends AbstractComponentResourceProvider {
-
-	@Override
-	public String[] getCssPath(final PageParameter pp) {
-		return new String[] { getCssResourceHomePath(pp) + "/imageslide.css" };
-	}
+public class ImageSlideResourceProvider extends AbstractComponentUIResourceProvider {
 
 	@Override
 	public String[] getDependentComponents(final PageParameter pp) {
--- src/main/java/net/simpleframework/mvc/component/ui/slider/SliderResourceProvider.java
+++ src/main/java/net/simpleframework/mvc/component/ui/slider/SliderResourceProvider.java
@@ -1,23 +1,19 @@
 package net.simpleframework.mvc.component.ui.slider;
 
 import net.simpleframework.mvc.PageParameter;
-import net.simpleframework.mvc.component.IComponentResourceProvider.AbstractComponentResourceProvider;
+import net.simpleframework.mvc.component.ui.AbstractComponentUIResourceProvider;
 
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class SliderResourceProvider extends AbstractComponentResourceProvider {
+public class SliderResourceProvider extends AbstractComponentUIResourceProvider {
 
 	@Override
 	public String[] getJavascriptPath(final PageParameter pp) {
 		return new String[] { getResourceHomePath() + "/js/slider.js" };
-	}
-
-	@Override
-	public String[] getCssPath(final PageParameter pp) {
-		return new String[] { getCssResourceHomePath(pp) + "/slider.css" };
 	}
 }
